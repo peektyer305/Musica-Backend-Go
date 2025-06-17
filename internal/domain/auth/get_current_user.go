563fa2b3d266dfaeccbfd0d6f023af1b6edc9a1a
akira <pektyer305@gmail.com>
2025-06-17 18:27:47 +0900

    add

--- internal/domain/auth/get_current_user.go
+++ internal/domain/auth/get_current_user.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"errors"
+
 	domain "Musica-Backend/internal/domain/auth"
 )
 
@@ -11,4 +13,32 @@ type GetCurrentUserUseCase interface {
 type getCurrentUserInteractor struct {
 	jwtService     domain.JWTService
 	userRepository domain.IUserRepository
+}
+
+func NewGetCurrentUserUsecase(
+	jwtSvc domain.JWTService,
+	userRepo domain.IUserRepository,
+) GetCurrentUserUseCase {
+	return &getCurrentUserInteractor{
+		jwtService:     jwtSvc,
+		userRepository: userRepo,
+	}
+}
+
+func (i *getCurrentUserInteractor) Execute(in *GetCurrentUserInput) (*GetCurrentUserOutput, error) {
+	claims, err := i.jwtService.Validate(in.Token)
+	if err != nil {
+		return nil, err
+	}
+	// Auth0 の 'sub' をユーザーIDとみなす
+	sub, ok := claims["sub"].(string)
+	if !ok {
+		return nil, errors.New("invalid token subject")
+	}
+	// オプション: ユーザリポジトリから詳細情報を取得
+	user, err := i.userRepository.FindByID(sub)
+	if err != nil {
+		return nil, err
+	}
+	return &GetCurrentUserOutput{User: user}, nil
 }
